fb7ca9d1c6e8db0603b1e6be001e2daafbb6a433
Alexandre Oliveira <aletoscaro@gmail.com>
2024-04-29 20:43:57 -0300

    build: adicionando cors para aplicação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,22 @@
 import fastify, { FastifyInstance } from "fastify";
 import { UserRoutes } from "./routes/user-routes";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify()
 
-app.listen({ port: 3333 }).then(() => {
-    console.log("HTTP server is running!!")
-})
+app.register(fastifyCors, {
+    // Configurações opcionais do CORS
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE']
+  });
 
 app.register(UserRoutes)
 
 app.register(fastifyJwt, {
 	secret: 'askjdkas 21873561872v787812t312 1267vsdrasd 121276fe612r37612r6712r867'
 });
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP server is running!!")
+})
