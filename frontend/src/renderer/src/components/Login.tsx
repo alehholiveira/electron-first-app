515c3db2625f433127652dcb9309e2676d64e39d
Alexandre <aletoscaro@gmail.com>
2024-04-30 11:11:52 -0300

    feat: formulario login funcionando corretamente

--- frontend/src/renderer/src/components/Login.tsx
+++ frontend/src/renderer/src/components/Login.tsx
@@ -1,18 +1,43 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiUser } from 'react-icons/bi'
 import { AiOutlineLock } from 'react-icons/ai'
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Login(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3333/login', {
+        email,
+        password,
+      });
+
+      localStorage.setItem('jwt', response.data.token);
+      console.log(response.data);
+      navigate('/users');
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
+  };
+
   return (
     <div>
       <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
         <h1 className="text-4xl text-white font-bold text-center mb-6">Login</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="relative my-4">
             <input
               type="Email"
+              value={email}
               className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearnce-none dark:focus:border-blue-500 focus: outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
               placeholder=""
+              onChange={(e) => setEmail(e.target.value)}
             />
             <label
               htmlFor=""
@@ -26,6 +51,8 @@ export default function Login(): JSX.Element {
             <input
               type="password"
               className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearnce-none dark:focus:border-blue-500 focus: outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <label
               htmlFor=""
